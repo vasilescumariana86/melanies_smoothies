0870974df7125fb60e740a682066f5ad34dc1c0e
vasilescumariana86 <vasilescu.mariana86@gmail.com>
2025-05-27 14:40:37 +0300

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write("Name is ",name_on_order)
 # )
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARC_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 options = st.multiselect(
